f6806186dd6831b9793465a839d8b8fa5099c3dd
Mohamed Raessa <mohamed.s.raessa@ieee.org>
2020-07-08 09:50:49 +0900

    remove unneeded comments

--- src/simple_arm/src/simple_mover.cpp
+++ src/simple_arm/src/simple_mover.cpp
@@ -1,37 +1,40 @@
-#include "ros/ros.h"
-#include "std_msgs/Float64.h"
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
 
 
-int main(int argc, char** argv){
-    
-    ros::init(argc, argv, "arm_mover");
+int main (int argc, char ** argv){
+
+    ros::init(argc, argv, "simple_mover2");
     ros::NodeHandle nh;
-    //pub1 and 2
 
-    ros::Publisher joint_1_pub = nh.advertise<std_msgs::Float64>("/simple_arm/joint_1_pos_controller/command", 10);
-    ros::Publisher joint_2_pub = nh.advertise<std_msgs::Float64>("/simple_arm/joint_2_pos_controller/command", 10);
+    ros::Publisher pub1 = nh.advertise<std_msgs::Float64>("/simple_arm/jnt_1_pos_controller/command", 10);
+    ros::Publisher pub2 = nh.advertise<std_msgs::Float64>("/simple_arm/jnt_2_pos_controller/command", 10);
 
     ros::Rate rate(10);
 
-    int start_time{0}, elapsed{0};
-    while(! start_time){
+    int start_time{0}, elapsed_time{0};
+
+    while (!start_time)
+    {
         start_time = ros::Time::now().toSec();
     }
 
-    std_msgs::Float64 joint_1_angle;
-    std_msgs::Float64 joint_2_angle;
+    std_msgs::Float64 jnt_1_val;
+    std_msgs::Float64 jnt_2_val;
 
-    while (ros::ok())
-    {
-        elapsed = ros::Time::now().toSec();
-        joint_1_angle.data = M_PI_2 * 0.1 *sin(2*M_PI*elapsed);
-        joint_1_angle.data = M_PI_2 * 0.1 *sin(2*M_PI*elapsed);
+    ROS_INFO("Started jnts publisher");
+
+    while(ros::ok()){
+        elapsed_time = ros::Time::now().toSec() - start_time;
+
+        jnt_1_val.data = M_PI_2 * sin(2*M_PI * elapsed_time);
+        jnt_2_val.data = M_PI_2 * sin(2*M_PI * elapsed_time);
         
-        joint_1_pub.publish(joint_1_angle);
-        joint_2_pub.publish(joint_2_angle);
+        pub1.publish(jnt_1_val);
+        pub2.publish(jnt_2_val);
 
         rate.sleep();
     }
-    
+
     return 0;
 }
